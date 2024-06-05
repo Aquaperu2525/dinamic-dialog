6c0cab9749e8c5ebe9bf4cac62db43d1325543ff
ingdantev <ingdantev@gmail.com>
2024-06-05 06:02:02 +0000

    first commit

---
+++ src/app/dynamicdialog/dynamicdialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamicdialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamicdialog.component.html',
+  styleUrl: './dynamicdialog.component.css'
+})
+export class DynamicdialogComponent {
+
+}
